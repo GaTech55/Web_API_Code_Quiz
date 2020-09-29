d196ff76b798d6fac71b398ba923c4cd82d55765
David Brown <gatech55@gmail.com>
2020-09-29 04:09:06 -0400

    added in 2nd page with question and buttons

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 // console.log("Hello World");
 var introContainer = document.getElementById("intro");
 var startButton = document.getElementById("startBtn");
+var questionVar = document.getElementById("question");
+var choicesVar = document.getElementById("choices");
+
+startButton.addEventListener("click", function () {
+  introContainer.style.display = "none";
+  showChoices();
+});
+
+function showChoices() {
+  var questionOne = "Commonly used data types DO NOT include:";
+  var availableChoices = [
+    "1. strings",
+    "2. booleans",
+    "3. alerts",
+    "4. numbers",
+  ];
+
+  for (var i = 0; i < availableChoices.length; i++) {
+    // console.log(availableChoices.length);
+    // Create element
+    var button = document.createElement("button");
+    // Add content
+    button.setAttribute("class", "btn btn-info");
+    button.textContent = availableChoices[i];
+    button.setAttribute("data-value", availableChoices[i]);
+    //Append to an existing element
+    choicesVar.append(button);
+  }
+  questionVar.setAttribute("class", "h1");
+  questionVar.append(questionOne);
+}
+
+choicesVar.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    console.log("You clicked a button");
+    var selectedChoice = event.target.getAttribute("data-value");
+    console.log(selectedChoice);
+  }
+});
